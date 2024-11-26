ee12a4b04a68592916ba06ad35e89d1e4fbab185
Jirachai C <jirachai.c@outlook.com>
2024-11-26 14:11:26 +0800

    Add type check for hashBigObject to handle non-object inputs

--- src/tools/hash.ts
+++ src/tools/hash.ts
@@ -5,6 +5,10 @@ export const hash = (str: string) => murmurhash.v3(str).toString()
 export const hashObject = (obj: any) => hash(JSON.stringify(obj))
 
 export const hashBigObject = (obj: any): string => {
+  if (typeof obj !== 'object') {
+    return 'not an object'
+  }
+
   const hashArray = Object.keys(obj)
     // We sort the keys to ensure the hash is consistent
     .sort()
